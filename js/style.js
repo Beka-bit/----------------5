1ec396854220ce2ac8b36415d1e31f8714bac8a3
Bekarys <b6134991@gmail.com>
2024-11-25 11:56:17 +0600

    Қортынды емтихан5

---
+++ js/style.js
@@ -0,0 +1,42 @@
+// task1
+// let san1=+prompt("1 Sandy engiz")
+// let san2=+prompt("2 sandy engiz")
+// function barlikNatizhe(san1,san2){
+// console.log("Azaitkanda: " + (san1-san2));
+// san1-san2
+// console.log("Koskanda: " + (san1+san2));
+// san1+san2
+// console.log("kobeitkende: " + (san1*san2));
+// san1*san2
+// console.log("Bolgende: " + (san1/san2));
+// san1/san2
+// }
+// barlikNatizhe(san1,san2);
+
+
+// task 2 
+// let total = 0;
+// while(true){
+//     i=1;
+//     let tovar = +prompt("Қанша тауар сатып алғаназ келеді")
+//     if(){
+
+//     }
+//     i++;
+// }
+
+
+
+
+// tesk 3
+// let san=25;
+// while(true){
+//     let jayap = +prompt("Жасырылған санды тап 0 - 100 арасында")
+//     if(jayap===san){
+//         alert("Siz sandy taptynyz!")
+//     }else if(jayap-5>jayap){
+//         alert("Сіз жақын сан еңгіздініз.Сәл жоғары")
+//     }else if(jayap+5<jayap){
+//         alert("Сіз жақын сан еңгіздініз.Сәл Төмен")
+//     }else if(jayap>=jayap)
+// }
